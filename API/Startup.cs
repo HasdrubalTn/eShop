bc3cd257814a5c7b452ecb7fb15d7d90fbf9e26a
Ali MSELMI <mselmi@outlook.com>
2020-09-20 11:35:53 +0200

    Add Cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,16 @@ namespace API
 
             services.ApplicationServices();
             services.AddSwaggerDocumentation();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +54,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors();
             app.UseAuthorization();
 
             app.UseSwaggerDocumentation();

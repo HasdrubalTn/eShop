4bb46b734c31e51ce5446a361e38af47047f07db
Ali MSELMI <mselmi@outlook.com>
2020-09-23 00:49:08 +0200

    Error handling

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -15,7 +15,11 @@ export class ErrorInterceptor  implements HttpInterceptor {
       catchError(error => {
         if (error) {
           if (error.status === 400) {
-            this.toastr.error(error.error.message, error.error.statusCode);
+            if (error.error.errors){
+              throw error.error;
+            }else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
           }
           if (error.status === 401) {
             this.toastr.error(error.error.message, error.error.statusCode);
@@ -24,7 +28,8 @@ export class ErrorInterceptor  implements HttpInterceptor {
             this.router.navigateByUrl('/not-found');
           }
           if (error.status === 500) {
-            this.router.navigateByUrl('/server-error');
+            const navigationExtras: NavigationExtras = {state: {error: error.error}};
+            this.router.navigateByUrl('/server-error', navigationExtras);
           }
         }
         return throwError(error);
